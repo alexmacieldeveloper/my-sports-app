890d3ca1d44d129b3d3b97054d42118d10e9ae0c
Álex Maciel <aweasley.gina@gmail.com>
2025-09-06 20:47:17 -0300

    :sparkles: feat: adding domain of images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["media.api-sports.io"], // domínio das logos
+  },
 };
 
 export default nextConfig;
