390d5c32515175e96057a1f96420a977035e9e42
Álex Maciel <aweasley.gina@gmail.com>
2025-09-06 23:00:28 -0300

    :sparkleS: feat: creating types

---
+++ src/types/types.ts
@@ -0,0 +1,70 @@
+export interface ApiLeagueItem {
+  league: {
+    id: number;
+    name: string;
+    logo: string;
+  };
+  country: {
+    name: string;
+    code?: string;
+    flag?: string;
+  };
+  season: number;
+}
+
+export interface LeagueData {
+  name: string;
+  country?: string;
+  season?: number;
+  logo?: string;
+}
+
+export interface ApiMatchItem {
+  fixture: {
+    id: number;
+    date: string; 
+    status: {
+      short: string;
+      long?: string;
+    };
+  };
+  league: {
+    id: number;
+    name: string;
+    logo: string;
+    country?: {
+      name: string;
+      code?: string;
+    };
+    season: number;
+  };
+  teams: {
+    home: {
+      id: number;
+      name: string;
+      logo: string;
+    };
+    away: {
+      id: number;
+      name: string;
+      logo: string;
+    };
+  };
+  score: {
+    fulltime: {
+      home?: number;
+      away?: number;
+    };
+  };
+}
+
+export interface MatchData {
+  id: number;           
+  date: string;         
+  homeTeamName: string;
+  awayTeamName: string;
+  leagueName: string;
+  status?: string;
+  scoreHome?: number;
+  scoreAway?: number;
+}
